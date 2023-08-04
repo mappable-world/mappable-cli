68064d919e450d9fcd96216a77311e7066a1a1af
xdan <chupurnov@gmail.com>
2023-08-04 14:29:23 +0300

    New version 0.0.1-beta.39 Read more https://github.com/mappable-world/mappable-cli/releases/tag/0.0.1-beta.39

--- tools/replace.middleware.js
+++ tools/replace.middleware.js
@@ -2,6 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const stream = require('stream');
 
+const EXT_MIME = {
+    js: 'application/javascript',
+    json: 'application/json',
+    html: 'text/html',
+    css: 'text/css',
+    svg: 'image/svg+xml',
+    png: 'image/png',
+    jpg: 'image/jpeg',
+    jpeg: 'image/jpeg'
+};
+
 module.exports.replaceMiddleware = (dir) => (middleares, devServer) => {
     devServer.app.get('/*', (req, res, next) => {
         const file = path.join(dir, req.url);
@@ -16,15 +27,18 @@ module.exports.replaceMiddleware = (dir) => (middleares, devServer) => {
                 `<html lang="en">
                    <body>
                     <ul>${fs
-                    .readdirSync(file)
-                    .map((p) => `<li><a href="${path.join(req.path, p)}">${p}</a></li>`)
-                    .join('')}
+                        .readdirSync(file)
+                        .map((p) => `<li><a href="${path.join(req.path, p)}">${p}</a></li>`)
+                        .join('')}
                     </ul>
                    </body>
                </html>`
             );
             return;
         }
+
+        const extMatch = pathname.match(/(?<=\.)\w+$/);
+        const mime = (extMatch && EXT_MIME[extMatch[0]]);
 
         let rs = fs.createReadStream(file);
         if (/\.html?$/.test(req.url)) {
@@ -38,6 +52,8 @@ module.exports.replaceMiddleware = (dir) => (middleares, devServer) => {
             );
         }
         rs.pipe(res);
+
+        mime && res.set({'Content-Type': mime});
     });
 
     return middleares;
