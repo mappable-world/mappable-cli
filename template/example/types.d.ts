92386bc42288887435221e8ef716ab5c6e999701
Matthew Kalinin <matthew44@mappable.world>
2024-03-11 11:24:01 +0300

    add override for import types

--- template/example/types.d.ts
+++ template/example/types.d.ts
@@ -11,10 +11,4 @@ declare global {
     }
 }
 
-declare module '@mappable-world/mappable-types/import' {
-    interface Import {
-        (pkg: '%PACKAGE_NAME%'): Promise<typeof import('../src/index')>;
-    }
-}
-
 export {};
